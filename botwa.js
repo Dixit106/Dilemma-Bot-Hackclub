d2279138ad31d939109015d276dab7b2a0e00ff6
Dixit106 <dixitk2958@gmail.com>
2026-09-03 22:08:19 +0530

    make my tit for tat bot more smarter

---
+++ botwa.js
@@ -0,0 +1,21 @@
+export default function bot({ history, memory }) {
+    if (memory === null) {
+        memory = { betrayals: 0 };
+    }
+
+    if (history.length === 0) {
+        return ["C", memory];
+    }
+
+    const lastOpponentMove = history.at(-1).opponent;
+
+    if (lastOpponentMove === "D") {
+        memory.betrayals += 1;
+    }
+
+    if (memory.betrayals >= 3) {
+        return ["D", memory];
+    }
+
+    return [lastOpponentMove, memory];
+}
