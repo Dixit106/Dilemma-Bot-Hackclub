edd68b644811132d28b3c6f05cfd79f02836a172
Dixit106 <dixitk2958@gmail.com>
2026-09-04 22:33:21 +0530

    added more logic and maths to reach top in leaderboard and my bot could have advantage with every type of bot on somewhat level

--- botwa.js
+++ botwa.js
@@ -1,34 +1,54 @@
 export default function bot({ history, memory }) {
 
-    memory = memory ?? { opStrikes: 0, weTested: false, doNotTest: false };
+    memory = memory ?? { opStrikes: 0, state: "normal" };
 
     if (history.length === 0) return ["C", memory];
 
     const opLast = history.at(-1).opponent;
     if (opLast === "D") memory.opStrikes++;
 
-    if (memory.weTested && opLast === "D") {
-        memory.weTested = false;
-        memory.doNotTest = true;
+    if (memory.state === "testing" && opLast === "D") {
+        memory.state = "respect";
         return ["C", memory];
     }
 
-    memory.weTested = false;
-
-    if (history.length > 1){
-        const myLast = history.at(-1).you;
+     if (history.length > 1){
         const opPrev = history.at(-2).opponent;
         const myPrev = history.at(-2).you;
-        if (myLast === "D" && opLast === "D" && myPrev === "D" && opPrev === "D") {
+        if (myPrev === "D" && opPrev === "D" && opLast === "C") {
             return ["C", memory];
         }
     }
 
-    if (opLast === "D") return ["D", memory];
+    if (opLast === "D") {
+        const provoked = history.length > 1 && history.at(-2).you === "D";
 
-    if (!memory.doNotTest && history.length % 10 === 0 && memory.opStrikes === 0) {
-        memory.weTested = true;
+        if (provoked) {
+            return ["C", memory];
+        }
+
         return ["D", memory];
+    }
+
+    if (memory.state === "farming") {
+        if (opLast === "D") {
+            memory.state = "respect";
+            return ["C", memory];
+        }
+        const len = history.length;
+        if (len === 23 || len === 27 || len === 53 || len === 71 || len === 89 || len === 107) {
+            return ["D", memory];
+        }
+        return ["C", memory];
+    }
+
+    if (memory.state === "normal" && history.length === 15 && memory.opStrikes === 0) {
+        memory.state = "testing";
+        return ["D", memory];
+    }
+
+    if (memory.state === "testing" && opLast === "C") {
+        memory.state = "farming";
     }
 
     return ["C", memory];
